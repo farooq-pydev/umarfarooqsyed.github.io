521b5f77216b76374122a29465b8e60d73c583dc
Umar Farooq <umarfarook406@gmail.com>
2026-03-20 15:36:14 +0530

    changes

--- script.js
+++ script.js
@@ -6,17 +6,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-VANTA.WAVES({
-  el: "#vanta-bg",
-  color: 0x2563eb, // nebula blue
-  shininess: 50,
-  waveHeight: 20,
-  waveSpeed: 1
-});
-
 document.addEventListener("mousemove", e => {
   document.body.style.setProperty('--x', e.clientX + 'px');
   document.body.style.setProperty('--y', e.clientY + 'px');
   document.body.style.setProperty('transform', `translate(${e.clientX}px, ${e.clientY}px)`);
+});
+
+VANTA.WAVES({
+  el: "#bg-animation",
+  mouseControls: true,
+  touchControls: true,
+  color: 0x2563eb,
+  shininess: 50,
+  waveHeight: 20,
+  waveSpeed: 1
 });
 
