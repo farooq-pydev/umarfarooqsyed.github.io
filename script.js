7f0055f956ee66986ee6dcfa3dc7100ce43ad897
Umar Farooq <umarfarook406@gmail.com>
2026-03-20 15:13:51 +0530

    made changes to get grafical

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       .scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+VANTA.WAVES({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  color: 0x2563eb, // nebula blue
+  shininess: 50,
+  waveHeight: 20,
+  waveSpeed: 1
+});
+
+document.addEventListener("mousemove", e => {
+  document.body.style.setProperty('--x', e.clientX + 'px');
+  document.body.style.setProperty('--y', e.clientY + 'px');
+  document.body.style.setProperty('transform', `translate(${e.clientX}px, ${e.clientY}px)`);
+});
+
